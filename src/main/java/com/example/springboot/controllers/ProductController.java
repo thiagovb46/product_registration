8f540a6187ac229d2fb881424250d8c88123cc80
Thiago Vasconcelos Braga <thiagovb46@gmail.com>
2023-12-17 14:48:06 -0300

    Add Put End-Point

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -39,10 +40,20 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
-    @DeleteMapping("/products")
-    public ResponseEntity deleteOne(@RequestParam UUID productId){
-        productRepository.deleteById(productId);
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity deleteOne(@PathVariable(value = "id") UUID id){
+        productRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @PutMapping("/products/{id}")
+    public ResponseEntity<ProductModel> updateOne(@PathVariable (value = "id") UUID id, @RequestBody ProductRecordDto productDto){
+        var product = productRepository.findById(id);
+        if(product.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        var productModel = product.get();
+        BeanUtils.copyProperties(productDto, productModel);
+        productRepository.save(productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productModel);
     }
 
 }
