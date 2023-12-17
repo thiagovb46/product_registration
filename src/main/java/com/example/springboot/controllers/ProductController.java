ebdbe6c0749407af57535fb5a93ae0228d7e9b07
Thiago Vasconcelos Braga <thiagovb46@gmail.com>
2023-12-17 14:29:31 -0300

    Add new end-points

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -4,15 +4,12 @@ import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
